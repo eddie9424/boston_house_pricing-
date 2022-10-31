0aa784c23f0dc7951fd339e44b653433687926cc
eddie9424 <abdellah.moadine.etu@univ-lille.fr>
2022-10-31 01:04:57 +0100

    commit for web application, to be updated soon

---
+++ application.py
@@ -0,0 +1,42 @@
+from pydoc import importfile
+from pyexpat import model
+import re
+import numpy as np
+import pandas as pd
+import pickle
+from flask import Flask , request, app, jsonify , url_for, render_template 
+import sklearn  
+
+
+app=Flask(__name__)
+
+
+odel = pickle.load(open('regression_model_1.pkl','rb'))
+
+scalar = pickle.load(open('scaling.pkl','rb'))
+
+
+@app.route('/')
+
+
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict_api', methods=['POST'])
+
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output = odel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
